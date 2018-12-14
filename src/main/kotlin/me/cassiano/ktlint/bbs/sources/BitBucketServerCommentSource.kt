d905d9f5f266a3f7efa11c78db9e35eb06f11b6c
Matheus C. Candido <matheus.candido@viavarejo.com.br>
2018-12-14 17:22:28 -0200

    Removing image markdown

--- src/main/kotlin/me/cassiano/ktlint/bbs/sources/BitBucketServerCommentSource.kt
+++ src/main/kotlin/me/cassiano/ktlint/bbs/sources/BitBucketServerCommentSource.kt
@@ -8,7 +8,6 @@ import me.cassiano.ktlint.bbs.core.Issue
 class BitBucketServerCommentSource : CommentSource {
 
     override fun getComment(issue: Issue): Comment {
-//        val pikachu = "[![46482723-914713205554017-4832741937838555136-n-cke.jpg](attachment:896/a960a6b976%2F46482723-914713205554017-4832741937838555136-n-cke.jpg)](attachment:896/a960a6b976%2F46482723-914713205554017-4832741937838555136-n-cke.jpg)"
         return Comment("(${issue.line}, ${issue.column}): ${issue.message}", issue.line, issue.path)
     }
 
